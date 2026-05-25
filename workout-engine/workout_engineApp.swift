b4e55d43979cde9da8e2c85eacd522bcd04ef574
iufb <bashirov3ld2@gmail.com>
2026-05-25 19:43:27 +0500

    Initial Commit

---
+++ workout-engine/workout_engineApp.swift
@@ -0,0 +1,17 @@
+//
+//  workout_engineApp.swift
+//  workout-engine
+//
+//  Created by iufb on 25.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct workout_engineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
